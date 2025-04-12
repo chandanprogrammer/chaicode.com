49a0321076212c8d347a073dd604f86457b7a176
chandan kumar <chandankumar070501@gmail.com>
2025-04-12 06:01:52 +0530

    font color change

--- frontend/src/components/WhyChaiCode.jsx
+++ frontend/src/components/WhyChaiCode.jsx
@@ -14,30 +14,30 @@ const WhyChaiCode = () => {
 
       <div className="grid grid-cols-1 lg:grid-cols-3 w-full mt-8 gap-6">
         <div className="h-full flex flex-col gap-4 items-center justify-between">
-          <div className="bg-gray-800 p-6 rounded-2xl shadow-lg border hover:shadow-xl transform hover:scale-105 transition-all duration-300 ease-in-out text-center cursor-pointer">
-            <h3 className="text-lg font-semibold text-orange-500 mb-2 ">
+          <div className="bg-gray-900/90 p-6 rounded-2xl shadow-lg border border-orange-100/25 hover:shadow-xl transform hover:scale-105 transition-all duration-300 ease-in-out text-center cursor-pointer">
+            <h3 className="text-[20px] font-semibold text-orange-300/70 mb-4 tracking-wide">
               Comprehensive Curriculum
             </h3>
-            <p className="text-sm text-gray-400">
+            <p className="text-[14px] tracking-wide text-gray-400/70">
               Master key concepts and hands-on skills with clarity and
               confidence. Learn what truly matters, the right way, thorough,
               parctical, and easy to understrand.
             </p>
           </div>
-          <div className="bg-gray-800 p-6 rounded-2xl shadow-lg border hover:shadow-xl transform hover:scale-105 transition-all duration-300 ease-in-out text-center cursor-pointer">
-            <h3 className="text-lg font-semibold text-orange-500 mb-2 ">
+          <div className="bg-gray-900/90 p-6 rounded-2xl shadow-lg border border-orange-100/25 hover:shadow-xl transform hover:scale-105 transition-all duration-300 ease-in-out text-center cursor-pointer">
+            <h3 className="text-[20px] font-semibold text-orange-300/70 mb-4 tracking-wide ">
               You finish it
             </h3>
-            <p className="text-sm text-gray-400">
+            <p className="text-[14px] tracking-wide text-gray-400/70">
               Our cohorts are a collaborative journay, students learn together,
               saty motivated, and complete the course on time as a community
             </p>
           </div>
-          <div className="bg-gray-800 p-6 rounded-2xl shadow-lg border hover:shadow-xl transform hover:scale-105 transition-all duration-300 ease-in-out text-center cursor-pointer">
-            <h3 className="text-lg font-semibold text-orange-500 mb-2 ">
+          <div className="bg-gray-900/90 p-6 rounded-2xl shadow-lg border border-orange-100/25 hover:shadow-xl transform hover:scale-105 transition-all duration-300 ease-in-out text-center cursor-pointer">
+            <h3 className="text-[20px] font-semibold text-orange-300/70 mb-4 tracking-wide ">
               Industry Guests
             </h3>
-            <p className="text-sm text-gray-400">
+            <p className="text-[14px] tracking-wide text-gray-400/70">
               We're connected with indestry experts and regularly bring them
               into our classes for engaging, fun, and insightful sessions with
               students.
@@ -45,20 +45,20 @@ const WhyChaiCode = () => {
           </div>
         </div>
         <div className="flex items-center justify-between">
-          <div className="h-full bg-[#101426] p-4 rounded-2xl shadow-lg border hover:shadow-xl transform hover:scale-105 transition-all duration-300 ease-in-out text-center cursor-pointer flex flex-col items-center justify-center">
+          <div className="h-full bg-[#101426] p-4 rounded-2xl shadow-lg border border-orange-100/25 hover:shadow-xl transform hover:scale-105 transition-all duration-300 ease-in-out text-center cursor-pointer flex flex-col items-center justify-center">
             <img src="./images/hitesh.png" alt="" />
-            <h2 className="text-3xl font-semibold text-gray-100/90 my-4">
+            <h2 className="text-3xl font-semibold text-cyan-300/80 my-4">
               Hitesh Choudhary
             </h2>
-            <p className="text-base text-cyan-300/60">
+            <p className="text-[15px] text-gray-400/70">
               Retired from corporate and full time Youtuber, x founder of
               LOC(acquired), x CTO, Sr. Director at PW. 2 Youtube Channels (950k
               & 470k), stepped into 43 countries.
             </p>
-            <h3 className="text-2xl font-semibold text-gray-100 my-3">
+            <h3 className="text-2xl font-semibold text-cyan-300/70 my-3">
               Approach
             </h3>
-            <p className="text-base text-gray-300/80">
+            <p className="text-base text-gray-300/60">
               Project based cources with peer learning and bounties with many
               activities.
             </p>
@@ -99,31 +99,31 @@ const WhyChaiCode = () => {
         </div>
         <div className="flex items-center justify-center">
           <div className=" h-full flex flex-col gap-4 items-center justify-between">
-            <div className="bg-gray-800 p-6 rounded-2xl shadow-lg border hover:shadow-xl transform hover:scale-105 transition-all duration-300 ease-in-out text-center cursor-pointer">
-              <h3 className="text-lg font-semibold text-orange-500 mb-2 ">
+            <div className="bg-gray-900/90 p-6 rounded-2xl shadow-lg border border-orange-100/25 hover:shadow-xl transform hover:scale-105 transition-all duration-300 ease-in-out text-center cursor-pointer">
+              <h3 className="text-[20px] font-semibold text-orange-300/70 mb-4 tracking-wide ">
                 Code and Chill
               </h3>
-              <p className="text-sm text-gray-400">
+              <p className="text-[14px] tracking-wide text-gray-400/70">
                 Coding should be fun, not frightening. It might feel tought at
                 first, but with time and practice, everything starts to click
                 and fall into place.
               </p>
             </div>
-            <div className="bg-gray-800 p-6 rounded-2xl shadow-lg border hover:shadow-xl transform hover:scale-105 transition-all duration-300 ease-in-out text-center cursor-pointer">
-              <h3 className="text-lg font-semibold text-orange-500 mb-2 ">
+            <div className="bg-gray-900/90 p-6 rounded-2xl shadow-lg border border-orange-100/25 hover:shadow-xl transform hover:scale-105 transition-all duration-300 ease-in-out text-center cursor-pointer">
+              <h3 className="text-[20px] font-semibold text-orange-300/70 mb-4 tracking-wide ">
                 Improve Communication
               </h3>
-              <p className="text-sm text-gray-400">
+              <p className="text-[14px] tracking-wide text-gray-400/70">
                 One of the best ways to boosts communication skills is through
                 practice. Our peer classes make it happen whwre co-learners
                 tech, share, and grow together.
               </p>
             </div>
-            <div className="bg-gray-800 p-6 rounded-2xl shadow-lg border hover:shadow-xl transform hover:scale-105 transition-all duration-300 ease-in-out text-center cursor-pointer">
-              <h3 className="text-lg font-semibold text-orange-500 mb-2 ">
+            <div className="bg-gray-900/90 p-6 rounded-2xl shadow-lg border border-orange-100/25 hover:shadow-xl transform hover:scale-105 transition-all duration-300 ease-in-out text-center cursor-pointer">
+              <h3 className="text-[20px] font-semibold text-orange-300/70 mb-4 tracking-wide ">
                 Bounties
               </h3>
-              <p className="text-sm text-gray-400">
+              <p className="text-[14px] tracking-wide text-gray-400/70">
                 Every cohorts comes with exciting cash prizes and some even
                 feature a MacBook giveway. It's our way of keeping the
                 motivation high and the learning fun.
